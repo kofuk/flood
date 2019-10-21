9561699d6f177c154b865d26011a44e23f98fe6b
Koki Fukuda <ko.fu.dev@gmail.com>
2019-10-21 12:00:43 +0900

    Implement drug

--- public/js/map.js
+++ public/js/map.js
@@ -20,6 +20,8 @@ const redisplay = () => {
 
     const ctxt = document.getElementById('map').getContext('2d');
 
+    ctxt.clearRect(0, 0, width, height);
+
     let bottommost = -offsetY;
     let rightmost = -offsetX;
 
@@ -36,11 +38,15 @@ const redisplay = () => {
 
             bottommost += 175;
         }
-        bottommost = 0;
+        bottommost = -offsetY;
         rightmost += 248;
     }
 
     requestAnimationFrame(redisplay);
+};
+
+const postRedisplay = () => {
+    needRedisplay = true;
 };
 
 const loadMap = (chunkName) => {
@@ -50,6 +56,10 @@ const loadMap = (chunkName) => {
     maps.set(chunkName, img);
 };
 
+let canvasRect;
+
+let endCond = {};
+
 const adjustCanvas = () => {
     const canvas = document.getElementById('map');
 
@@ -58,14 +68,95 @@ const adjustCanvas = () => {
 
     canvas.width = width;
     canvas.height = height;
+
+    //TODO: (screen width) > (map width)
+    endCond.chunkX = 18 - Math.floor(width / 248);
+    endCond.offsetX = width % 248;
+    //TODO: (screen height) > (map height)
+    endCond.chunkY = 20 - Math.floor(height / 175);
+    endCond.offsetY = height % 175;
+
+    canvasRect = canvas.getBoundingClientRect();
 };
 
-const postRedisplay = () => {
-    needRedisplay = true;
+let mouseDownP;
+let prevX, prevY;
+
+const mouseDown = (e) => {
+    mouseDownP = true;
+    prevX = e.clientX - canvasRect.left;
+    prevY = e.clientY - canvasRect.top;
+};
+
+const mouseMove = (e) => {
+    if (!mouseDownP) return;
+
+    const currentX = e.clientX - canvasRect.left;
+    const currentY = e.clientY - canvasRect.top;
+
+    const moveX = prevX - currentX;
+    const moveY = prevY - currentY;
+
+    offsetX += moveX;
+    offsetY += moveY;
+
+    for (;;) {
+        if (chunkX < endCond.chunkX && offsetX >= 248) {
+            offsetX -= 248;
+            ++chunkX;
+        } else if (chunkX > 0 && offsetX < 0) {
+            offsetX += 248;
+            --chunkX;
+        } else {
+            if (chunkX <= 0 && offsetX < 0) {
+                chunkX = 0;
+                offsetX = 0;
+            } else if (chunkX >= endCond.chunkX && offsetX > endCond.offsetX) {
+                chunkX = endCond.chunkX;
+                offsetX = endCond.offsetX;
+            }
+
+            break;
+        }
+    }
+
+    for (;;) {
+        if (chunkY < endCond.chunkY && offsetY >= 175) {
+            offsetY -= 175;
+            ++chunkY;
+        } else if (chunkY > 0 && offsetY < 0) {
+            offsetY += 175;
+            --chunkY;
+        } else {
+            if (chunkY <= 0 && offsetY < 0) {
+                chunkY = 0;
+                offsetY = 0;
+            } else if (chunkY >= endCond.chunkY && offsetY > endCond.offsetY) {
+                chunkY = endCond.chunkY;
+                offsetY = endCond.offsetY;
+            }
+
+            break;
+        }
+    }
+
+    postRedisplay();
+
+    prevX = currentX;
+    prevY = currentY;
+}
+
+const mouseUp = () => {
+    mouseDownP = false;
 };
 
 addEventListener('load', () => {
     adjustCanvas();
+
+    const canvas = document.getElementById('map');
+    canvas.addEventListener('mousedown', mouseDown);
+    canvas.addEventListener('mousemove', mouseMove);
+    canvas.addEventListener('mouseup', mouseUp);
 
     requestAnimationFrame(redisplay);
 });
